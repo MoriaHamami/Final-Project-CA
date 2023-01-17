a80a499fe630cf5be6d6d437818663393ea4b38c
Guy Agam <Guyos1997@gmail.com>
2023-01-17 20:24:03 +0200

    guy Check

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom'
 
 
 export function HomePage() {
-   
+
 
     return (
         <section>
             <h2>this is home page</h2>
+            <span>asfwegfsdvsfgdsf</span>
             <Link to="/templates">
-                    <button>Get Started</button>
-                </Link>
+                <button>Get Started</button>
+            </Link>
         </section >
     )
 }
