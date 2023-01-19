112d7b31612b20afbe11691dfcf9fc5880efd22a
Maor Aknin <maoraknin125@gmail.com>
2023-01-19 21:06:57 +0200

    added mini header in editor

---
+++ frontend/src/cmps/editor/editor-header.jsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShare, faReply, faDisplay, faTabletScreenButton, faMobileScreenButton, faUserGroup } from '@fortawesome/free-solid-svg-icons'
+
+export function EditorHeader() {
+
+    return (
+        <header className="editor-header">
+            <div className="editor-actions">
+                <FontAwesomeIcon className='editor-action' icon={faUserGroup} />
+                <FontAwesomeIcon className='editor-action' icon={faReply} />
+                <FontAwesomeIcon className='editor-action' icon={faShare} />
+                <FontAwesomeIcon className='editor-action' icon={faDisplay} />
+                <FontAwesomeIcon className='editor-action' icon={faTabletScreenButton} />
+                <FontAwesomeIcon className='editor-action' icon={faMobileScreenButton} />
+            </div>
+            <div className="site-actions">
+                <button className='preview-btn'>PREVIEW</button>
+                <button className='publish-btn'>PUBLISH</button>
+
+            </div>
+        </header >
+    )
+}
