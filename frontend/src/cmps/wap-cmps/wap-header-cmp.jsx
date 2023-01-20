18c03ec7414d9e0e00921c0f1c701e624a4dca40
Maor Aknin <maoraknin125@gmail.com>
2023-01-20 11:30:30 +0200

    start of responsiveness in board

--- frontend/src/cmps/wap-cmps/wap-header-cmp.jsx
+++ frontend/src/cmps/wap-cmps/wap-header-cmp.jsx
@@ -4,6 +4,7 @@ export function WapHeader({ style, cmp }) {
   return (
     <div id={cmp.type} style={style} className={cmp.name}>
         <p className="logo">{cmp.info.logo}</p>
+        <span className="material-symbols-outlined menu-btn">menu</span>
         <nav className="nav-bar">
         {cmp.info.btns?.map((btn) => {
           return <a href={btn.link} key={btn.id}>{btn.label}</a>
