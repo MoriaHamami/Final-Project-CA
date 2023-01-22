25e0caaaa04ed2096f4b2fe84b826afa6b53eb57
Moria Hamami <moriahamami@gmail.com>
2023-01-22 09:40:12 +0200

    Made video cmp editable

--- frontend/src/cmps/wap-cmps/wap-video-cmp.jsx
+++ frontend/src/cmps/wap-cmps/wap-video-cmp.jsx
@@ -2,14 +2,31 @@ import React from 'react'
 
 import { useSelector } from 'react-redux'
 
-export function VideoCmp({ style, cmp, onElementClick }) {
-    const selectedElementId = useSelector((storestate) => storestate.wapModule.selectedElementId)
+export function VideoCmp({ style, cmp, onElClick }) {
+    const selectedElement = useSelector((storestate) => storestate.wapModule.selectedElement)
     const info = cmp.info
 
     return (
-        <div id={cmp.type} style={style} className={cmp.name} data-container='parent' onClick={onElementClick}>
-            {cmp.info.title && <h2 data-container={info.title.id} onClick={onElementClick} style={info.title.style} className="title" contentEditable={selectedElementId === info.title.id} suppressContentEditableWarning={true} >{info.title.txt}</h2>}
-            {cmp.info.subtitle && <p data-container={info.subtitle.id} onClick={onElementClick} style={info.subtitle.style} className="subtitle" contentEditable={selectedElementId === info.subtitle.id} suppressContentEditableWarning={true} >{info.subtitle.txt}</p>}
+        <div id={cmp.type} style={style} className={cmp.name} data-container={JSON.stringify(cmp)} onClick={onElClick}>
+
+            {cmp.info.title && <h2
+                data-container={JSON.stringify(info.title)}
+                onClick={onElClick}
+                style={info.title.style}
+                className="title"
+                contentEditable={selectedElement?.id === info.title.id}
+                suppressContentEditableWarning={true} >
+                {info.title.txt}
+            </h2>}
+
+            {cmp.info.subtitle && <p data-container={JSON.stringify(info.subtitle)}
+                onClick={onElClick}
+                style={info.subtitle.style} className="subtitle"
+                contentEditable={selectedElement?.id === info.subtitle.id}
+                suppressContentEditableWarning={true} >
+                {info.subtitle.txt}
+            </p>}
+
             <div className="wap-card-video-container">
                 <iframe
                     style={cmp.info.iframeStyle}
