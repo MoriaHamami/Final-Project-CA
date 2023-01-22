04425f8e9511da7a18bc8d9e95ae7cb3fde750b4
Maor Aknin <maoraknin125@gmail.com>
2023-01-22 17:16:26 +0200

    Added editor sidebar styling

--- frontend/src/cmps/editor/sidebar-add.jsx
+++ frontend/src/cmps/editor/sidebar-add.jsx
@@ -5,26 +5,45 @@ import { Draggable } from "react-beautiful-dnd"
 import { wapService } from "../../services/wap.service"
 
 
-export function SidebarAdd({ onPickedCmp }) {
+export function SidebarAdd({ onPickedCmp, innerRef }) {
 
     const headerCmps = wapService.getCmpsByCategory('headers')
     // console.log('headerCmps:', headerCmps)
-    
-    return <div>
 
-        {headerCmps.map((cmp, index) => {
-            return <Draggable draggableId={cmp.id} key={cmp.id} index={index} >
-                {
-                    (provided, snapshot) => (
-                        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                            <img src={cmp.imgUrl} />
-                            {/* <div>{cmp.imgUrl}</div> */}
-                            {provided.placeholder}
-                        </div>
-                )
-                }
-            </Draggable>
-        })}
+    return <div className="add-main-container" ref={innerRef}>
+
+           <h2 className="add-header">Add section</h2>
+
+        <div className="add-container">
+
+        
+        <div className="add-side-bar">
+            <div>Headers</div>
+            <div>Heros</div>
+            <div>Previews</div>
+            <div>Maps</div>
+            <div>Videos</div>
+            <div>Forms</div>
+            <div>Footers</div>
+
+        </div>
+
+        <div className="elements-container">
+            {headerCmps.map((cmp, index) => {
+                return <Draggable draggableId={cmp.id} key={cmp.id} index={index} >
+                    {
+                        (provided, snapshot) => (
+                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                <img src={cmp.imgUrl} />
+                                {/* <div>{cmp.imgUrl}</div> */}
+                                {provided.placeholder}
+                            </div>
+                        )
+                    }
+                </Draggable>
+            })}
+        </div>
+        </div>
 
 
     </div>
