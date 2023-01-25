681ed4db5d88fa877abcb7d9f82a32d955517d41
Moria Hamami <moriahamami@gmail.com>
2023-01-25 19:02:41 +0200

    Add key to wap 5

--- frontend/src/waps/galleries/wap-gallery-5a.js
+++ frontend/src/waps/galleries/wap-gallery-5a.js
@@ -7,31 +7,34 @@ export const wapGallery5A = {
     imgUrl: 'https://res.cloudinary.com/dvnxslwcz/image/upload/v1674557656/Webix/wap-gallery-2a_zzwqzw.jpg',
 
     info: {
-        title: {txt: 'SERVICES', type: 'txt', id: utilService.makeId(), style: {color:'white', fontSize:'3.25rem'}},
-        subtitle: {txt: '', id: utilService.makeId(), type: 'txt', style: {}},
-        btn: {label: '', link: '', id: utilService.makeId(), type: 'btn', style: {}},
+        title: {txt: 'SERVICES', type: 'txt', id: utilService.makeId(), style: {color:'white', fontSize:'3.25rem'}, key: 'title'},
+        subtitle: {txt: '', id: utilService.makeId(), type: 'txt', style: {}, key: 'subtitle'},
+        btn: {label: '', link: '', id: utilService.makeId(), type: 'btn', style: {}, key: 'btn'},
         photos: [
             {
-                url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674661662/%D7%A9%D7%93%D7%92%D7%A9%D7%92_xeihm6.png',
-                title: { txt: 'EVENTS', id: utilService.makeId(), style: { fontFamily: 'playfair-regular'}, type: 'txt' },
-                subtitle: { txt: 'I\'m a title.Click here to add your own text and edit me.', id: utilService.makeId(), style: {}, type: 'txt' },
-                type: 'img',
-                id: utilService.makeId()
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674661662/%D7%A9%D7%93%D7%92%D7%A9%D7%92_xeihm6.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos'},
+                // url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674661662/%D7%A9%D7%93%D7%92%D7%A9%D7%92_xeihm6.png',
+                title: { txt: 'EVENTS', id: utilService.makeId(), style: { fontFamily: 'playfair-regular'}, type: 'txt', key: 'photos' },
+                subtitle: { txt: 'I\'m a title.Click here to add your own text and edit me.', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
+                // type: 'img',
+                // id: utilService.makeId()
             },
             {
-                url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674661664/%D7%93%D7%92%D7%9B%D7%93%D7%92%D7%9B_zcgszs.png',
-                title: { txt: 'CREATIVE', id: utilService.makeId(), style: { fontFamily: 'playfair-regular'}, type: 'txt' },
-                subtitle: { txt: 'I\'m a title. Click here to add your own text and edit me.', id: utilService.makeId(), style: {}, type: 'txt' },
-                type: 'img',
-                style: {},
-                id: utilService.makeId()
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674661664/%D7%93%D7%92%D7%9B%D7%93%D7%92%D7%9B_zcgszs.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos'},
+                // url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674661664/%D7%93%D7%92%D7%9B%D7%93%D7%92%D7%9B_zcgszs.png',
+                title: { txt: 'CREATIVE', id: utilService.makeId(), style: { fontFamily: 'playfair-regular'}, type: 'txt', key: 'photos' },
+                subtitle: { txt: 'I\'m a title. Click here to add your own text and edit me.', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
+                // type: 'img',
+                // style: {},
+                // id: utilService.makeId()
             },
             {
-                url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674661661/%D7%A9%D7%92%D7%A9%D7%93%D7%A9_op29bv.png',
-                title: { txt: 'FASHION', id: utilService.makeId(), style: { fontFamily: 'playfair-regular'}, type: 'txt' },
-                subtitle: { txt: 'I\'m a title.Click here to add your own text and edit me.', id: utilService.makeId(), style: {}, type: 'txt' },
-                type: 'img',
-                id: utilService.makeId()
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674661661/%D7%A9%D7%92%D7%A9%D7%93%D7%A9_op29bv.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos'},
+                // url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674661661/%D7%A9%D7%92%D7%A9%D7%93%D7%A9_op29bv.png',
+                title: { txt: 'FASHION', id: utilService.makeId(), style: { fontFamily: 'playfair-regular'}, type: 'txt', key: 'photos' },
+                subtitle: { txt: 'I\'m a title.Click here to add your own text and edit me.', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
+                // type: 'img',
+                // id: utilService.makeId()
             }
         ]
     },
