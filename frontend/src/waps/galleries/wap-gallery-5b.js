3a9ddf8d1bb0ea94fa1be83b577e39e84910718c
Maor Aknin <maoraknin125@gmail.com>
2023-01-25 19:02:29 +0200

    A

--- frontend/src/waps/galleries/wap-gallery-5b.js
+++ frontend/src/waps/galleries/wap-gallery-5b.js
@@ -7,47 +7,47 @@ export const wapGallery5B = {
     theme: 'theme-header-happy',
     thumbnail: 'https://i.postimg.cc/7h0qq8v4/wap-cards-1.png',
     info: {
-        title: {txt: '', id: utilService.makeId(), type: 'txt', style: {}, key: 'title'},
+        title: {txt: 'PORTFOLIO', id: utilService.makeId(), type: 'txt', style: {}, key: 'title'},
         subtitle: {txt: '', id: utilService.makeId(), type: 'txt', style: {},  key: 'subtitle'},
         btn: {label: '', link: '', id: utilService.makeId(), type: 'btn', style: {},  key: 'btn'},
         photos: [
             {
-                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674030131/card-img-1_tkzkif.webp', style: {}, id: utilService.makeId(),type: 'img', key: 'photos'},
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674663781/%D7%92%D7%99%D7%97%D7%9E%D7%A0%D7%94%D7%91%D7%92%D7%9B%D7%90%D7%98%D7%97_sqoyse.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos'},
                 title: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
                 subtitle: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
             },
             {
-                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674030130/card-img-7_u3vjoc.jpg', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674663781/%D7%A7%D7%A8%D7%90%D7%98%D7%97%D7%99%D7%A2%D7%9B%D7%92%D7%A8%D7%90%D7%98%D7%95%D7%97_fwtf1p.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
                 title: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
                 subtitle: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
             },
             {
-                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674030130/card-img-3_nt8pvb.jpg', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674663781/%D7%9F%D7%95%D7%98%D7%A2%D7%9B%D7%91_%D7%9E%D7%A6%D7%9C%D7%97%D7%99%D7%A2%D7%9B%D7%A2%D7%99_f0g0ty.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
                 title: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
                 subtitle: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
             },
             {
-                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674030130/card-img-8_a5pr2p.jpg', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674663781/%D7%9C%D7%97%D7%99%D7%A2%D7%9B%D7%92%D7%9B%D7%94%D7%A0%D7%9E%D7%A6%D7%9C%D7%A6%D7%9E%D7%A0_ao9gia.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
                 title: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
                 subtitle: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
             },
             {
-                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674030130/card-img-6_rxv4ii.jpg', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674663782/%D7%9F%D7%95%D7%98%D7%90%D7%A8%D7%92%D7%93%D7%92%D7%9B%D7%A2%D7%99%D7%97%D7%9C%D7%A6%D7%9E%D7%A0%D7%94%D7%91_cnrfci.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
                 title: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
                 subtitle: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
             },
             {
-                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674325042/pizza-cards2_qydcel.jpg', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674663781/%D7%A9%D7%93%D7%92%D7%A9%D7%93%D7%92%D7%A9%D7%93%D7%92%D7%A9%D7%93%D7%92_fscmqz.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
                 title: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
                 subtitle: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
             },
             {
-                photo: {url: 'https://res.cloudinary.com/tf-lab/image/upload/restaurant/51f12210-940b-465f-a6c4-8208da1330b9/468ab0f6-2c22-487d-b6d2-dd1f07433e9d.jpg', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674663781/%D7%A2%D7%9E%D7%9B%D7%A0%D7%94%D7%93%D7%92%D7%94%D7%9B%D7%92%D7%A2%D7%9E_g3ka1v.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
                 title: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
                 subtitle: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
             },
             {
-                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674325022/pizza-cards-img_f9db7g.webp', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
+                photo: {url: 'https://res.cloudinary.com/dimirmc9j/image/upload/v1674663781/%D7%92%D7%A0%D7%9B%D7%A0%D7%92%D7%9B%D7%A0%D7%94%D7%92%D7%94%D7%92%D7%9B%D7%94_hywpga.png', style: {}, id: utilService.makeId(),type: 'img', key: 'photos' },
                 title: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
                 subtitle: { txt: '', id: utilService.makeId(), style: {}, type: 'txt', key: 'photos' },
             },
